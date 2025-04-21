7b5af7d329f80144ce1ab25ce25536dabe80ea42
huangjinghui <jinghui.huang@spacemit.com>
2025-04-21 09:27:34 +0000

    add spacemit mm def
    Change-Id: I44968e54e17b763437e06984b54a6f2f6f418e6c

--- src/flag_gems/runtime/backend/_spacemit/__init__.py
+++ src/flag_gems/runtime/backend/_spacemit/__init__.py
@@ -4,22 +4,9 @@ from backend_utils import VendorInfoBase  # noqa: E402
 
 # from .heuristics_config_utils import HEURISTICS_CONFIGS
 
-
-global specific_ops, unused_ops
-specific_ops = None
-unused_ops = None
 vendor_info = VendorInfoBase(
     vendor_name="spacemit", device_name="cpu", device_query_cmd="lscpu"
 )
-
-
-# def OpLoader():
-#     global specific_ops, unused_ops
-#     if specific_ops is None:
-#         from . import ops  # noqa: F403
-
-#         specific_ops = ops.get_specific_ops()
-#         unused_ops = ops.get_unused_ops()
 
 class _DeviceGuard:
     def __init__(self, index: int):
--- src/flag_gems/runtime/backend/_spacemit/ops/__init__.py
+++ src/flag_gems/runtime/backend/_spacemit/ops/__init__.py
@@ -1,3 +1,4 @@
 from .gelu import gelu
+from .mm import mm
 
-__all__ = ["gelu"]
+__all__ = ["gelu", "mm"]
