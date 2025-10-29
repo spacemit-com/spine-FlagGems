176c1b390d688e181e1532224e592cabd3a0fe8c
zuoweixia <weixia.zuo@spacemit.com>
2025-10-29 03:23:29 +0000

    Refactor: mm to smt
    Change-Id: I7f8efa5de72c7e867059b06138d002b373c52589

--- src/flag_gems/runtime/backend/_spacemit/ops/mm.py
+++ src/flag_gems/runtime/backend/_spacemit/ops/mm.py
@@ -30,20 +30,13 @@ def mm_kernel(
     BLOCK_SIZE_N: tl.constexpr,
     BLOCK_SIZE_K: tl.constexpr,
     EVEN_K: tl.constexpr,
-    lhs_first: tl.constexpr,
-    mr: tl.constexpr,
-    nr: tl.constexpr,
-    kr: tl.constexpr,
-    mt: tl.constexpr,
-    nt: tl.constexpr,
-    kt: tl.constexpr,
-    mb: tl.constexpr,
-    nb: tl.constexpr,
-    kb: tl.constexpr,
+    SUB_BLK_M: tl.constexpr,
+    MICRO_M: tl.constexpr,
+    MICRO_K: tl.constexpr,
+    MICRO_N: tl.constexpr,
 ):
     pid_m = tl.program_id(0)
     pid_n = tl.program_id(1)
-
     a_block_ptr = tl.make_block_ptr(
         base=a_ptr,
         shape=[M, K],
@@ -62,22 +55,16 @@ def mm_kernel(
         order=[1, 0],
     )
 
-    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=tl.float32)
+    accumulator = tl.zeros((BLOCK_SIZE_M, BLOCK_SIZE_N), dtype=a_ptr.type.element_ty)
 
     if EVEN_K:
-        a = tl.load(a_block_ptr, boundary_check=(0, 1))
-        b = tl.load(b_block_ptr, boundary_check=(0, 1))
-        accumulator += tl.dot(a, b, allow_tf32=False)
-        smt.compile_hint(accumulator, "lhs_first", lhs_first)
-        smt.compile_hint(accumulator, "mr", mr)
-        smt.compile_hint(accumulator, "nr", nr)
-        smt.compile_hint(accumulator, "kr", kr)
-        smt.compile_hint(accumulator, "mt", mt)
-        smt.compile_hint(accumulator, "nt", nt)
-        smt.compile_hint(accumulator, "kt", kt)
-        smt.compile_hint(accumulator, "mb", mb)
-        smt.compile_hint(accumulator, "nb", nb)
-        smt.compile_hint(accumulator, "kb", kb)
+        b = smt.descriptor_load(b_block_ptr, (0, 0), (BLOCK_SIZE_K, BLOCK_SIZE_N), (MICRO_K, MICRO_N))
+        sub_num = (BLOCK_SIZE_M + SUB_BLK_M - 1)//SUB_BLK_M
+        for s in smt.parallel(0, sub_num):
+            if((M-BLOCK_SIZE_M*pid_m) > s*SUB_BLK_M):
+                a = smt.descriptor_load(a_block_ptr,  (s * SUB_BLK_M, 0), (SUB_BLK_M, BLOCK_SIZE_K), (MICRO_M, MICRO_K))
+                accumulator_view = smt.view(accumulator, (s * SUB_BLK_M, 0), (SUB_BLK_M, BLOCK_SIZE_N), (MICRO_M, MICRO_N))
+                accumulator_view  = smt.dot(a, b, accumulator_view)
     else:
         for k in range(0, tl.cdiv(K, BLOCK_SIZE_K)):
             a = tl.load(a_block_ptr, boundary_check=(0, 1))
@@ -118,10 +105,6 @@ def mm(a, b):
         triton.cdiv(N, META["BLOCK_SIZE_N"]),
     )
     BLOCK_SIZE_K = triton.next_power_of_2(K)
-    EVEN_K = 1
-    lhs_first = True
-    kr = BLOCK_SIZE_K
-    kt = 1
 
     mm_kernel[grid](
         a,
@@ -137,9 +120,5 @@ def mm(a, b):
         c.stride(0),
         c.stride(1),
         BLOCK_SIZE_K=BLOCK_SIZE_K,
-        EVEN_K=EVEN_K,
-        lhs_first=lhs_first,
-        kr=kr,
-        kt=kt,
     )
     return c
