55f64f4024619df8ea6c8dac9f3673d7fbb7dffe
zuoweixia <weixia.zuo@spacemit.com>
2026-05-22 11:39:34 +0800

    chore(spacemit): import missing utils import fixes for pointwise ops

--- src/flag_gems/runtime/backend/_spacemit/ops/pow.py
+++ src/flag_gems/runtime/backend/_spacemit/ops/pow.py
@@ -6,7 +6,7 @@ import triton.language as tl
 
 from flag_gems.utils import tl_extra_shim
 
-from ..utils.pointwise_dynamic import pointwise_dynamic
+from flag_gems.utils.pointwise_dynamic import pointwise_dynamic
 
 _pow = tl_extra_shim.pow
 logger = logging.getLogger(__name__)
